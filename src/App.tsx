c9db6807457f915e4849c5e8eb8837dff8ed50b3
Ahmad Alhasany <ahmadhs271@gmail.com>
2024-04-04 15:46:48 +0300

    Fix router by providing base

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Home from './Pages/Home/Home'
+import Home from './Pages/Home/Home';
 import Layout from './Components/Layout';
 import About from './Pages/About/About';
 import Blog from './Pages/Blog/Blog';
@@ -6,29 +6,30 @@ import Contact from './Pages/Contact/Contact';
 import Shop from './Pages/Shop/Shop';
 import Cart from './Pages/Cart/Cart';
 import NotFound from './Components/NotFound';
-
-import { RouterProvider, createRoutesFromElements, Route, createBrowserRouter } from 'react-router-dom';
 import ProductDets from './Pages/Shop/ProductDets';
 
-const router = createBrowserRouter(createRoutesFromElements(
-  <>   
-    <Route path='/' element={<Layout/>}>
-      <Route index element={<Home/>}/>
-      <Route path='/shop' element={<Shop/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/blog' element={<Blog/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/shop/:id' element={<ProductDets/>}/>
-    </Route>
-    <Route path="*" element={<NotFound/>} />
-  </>
-))
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
-
   return (
-        <RouterProvider router={router}/>
+    <Router basename="/Real-E-commerce">
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/shop/:id" element={<ProductDets />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+    </Router>
   );
 }
 
